6f2afa94c9569ac4d14fbb501faa77e0c338cfe9
andersonsnsz <andersonsnsz@gmail.com>
2024-08-17 09:36:49 -0300

    atualiza

--- analiseBD.R
+++ analiseBD.R
@@ -1,3 +1,11 @@
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("readxl")
+
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
+library(readxl)
+
+analisebd <- read_xlsx("C:/Users/ander/Desktop/Base Diaria.xlsx")
